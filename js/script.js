8a6a81ed07c1f092f30be251302b28f30746777e
HELAL15 <ah4459577@gmail.com>
2024-07-01 15:48:42 +0300

    finish project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-
+  let audio = new Audio('../images/music.mp3');
+  // Play the audio
+  audio.play();
   // Initialize Swiper for quotes
   const qoutes = new Swiper('.qoutes .swiper', {
     slidesPerView: 1,
@@ -19,6 +21,30 @@ $(document).ready(function() {
     },
   });
 
+
+  // Initialize Swiper for books2
+  const books2 = new Swiper('.books2 .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    allowTouchMove: false,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    },
+  });
+
+
+
+
   // Handle video play and pause controls
   $('.video-container').each(function() {
     const video = $(this).find('video').get(0);
@@ -30,7 +56,8 @@ $(document).ready(function() {
       video.play();
       videoControls.addClass('active')
       playControl.hide();
-      pauseControl.show();
+      pauseControl.css('display', 'flex');
+        audio.pause();
     });
 
     pauseControl.on('click', function() {
@@ -38,18 +65,21 @@ $(document).ready(function() {
       videoControls.removeClass('active')
       playControl.show();
       pauseControl.hide();
+        audio.play();
     });
 
     $(video).on('play', function() {
       videoControls.addClass('active')
       playControl.hide();
-      pauseControl.show();
+      pauseControl.css('display', 'flex');
+        audio.pause();
     });
 
     $(video).on('pause', function() {
       videoControls.removeClass('active')
       playControl.show();
       pauseControl.hide();
+        audio.play();
     });
   });
 
@@ -66,11 +96,13 @@ $(document).ready(function() {
       $(".hero-video").addClass("active")
       $(".play-video").hide()
       $(".pause-video").show()
+        audio.pause();
     } else {
       video.pause();
       $(".hero-video").removeClass("active")
       $(".play-video").show()
       $(".pause-video").hide()
+        audio.play();
     }
   });
 
@@ -89,7 +121,7 @@ $(".navbar-toggler-menu").on("click",()=>{
       const scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
 
       const scrollContentHeight = scrollContent.scrollHeight + scrollContainer.clientHeight;
-      scrollContainer.scrollTop = scrollContentHeight * scrollPercentage * 15;
+      scrollContainer.scrollTop = scrollContentHeight * scrollPercentage * 10;
   });
   
 
@@ -103,8 +135,6 @@ $(".navbar-toggler-menu").on("click",()=>{
   // Initialize WOW.js
   new WOW().init();
 
-  // Initialize niceSelect
-  $('select').niceSelect();
 
   // Scroll to top button functionality
   function showScrollTopBtn() {
@@ -125,17 +155,5 @@ $(".navbar-toggler-menu").on("click",()=>{
     });
   }
 
-  // Commented out navbar toggler functionality
-  // const navToggler = $('#nav-toggler');
-  // const collapse = $('.navbar-collapse');
-  // const close = $('#close');
-
-  // close.on('click', function() {
-  //   collapse.removeClass('active');
-  // });
-
-  // navToggler.on('click', function() {
-  //   collapse.toggleClass('active');
-  // });
 
 });
